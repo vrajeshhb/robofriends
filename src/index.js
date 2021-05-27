a89dc3d16387e3efccdefb8cf42ca3566c588874
rushirami07 <82886004+rushirami07@users.noreply.github.com>
2021-05-27 23:13:01 +0530

    updateone

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+                  <div>
+                    <div className="tc"><h1>my robot contact</h1> </div>
+                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+                    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />                                        
+                    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+                    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+                    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
+                    <Card id={robots[6].id} name={robots[6].name} email={robots[6].email} />                                        
+                    <Card id={robots[7].id} name={robots[7].name} email={robots[7].email} />
+                    <Card id={robots[8].id} name={robots[8].name} email={robots[8].email} />
+                  </div>
+                  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots =[
+	{
+		id:1,
+		name : 'Lucy Merry',
+		username: 'lucy01',
+		email: 'lucyMerry@hotmail.com'
+	},
+	{
+		id:2,
+		name : 'Rufus Tillery',
+		username: 'RufusTillery01',
+		email: 'RufusTillery@hotmail.com'
+	},
+	{
+		id:3,
+		name : 'Brad Paul',
+		username: 'BradPaul01',
+		email: 'BradPaul@hotmail.com'
+	},
+	{
+		id:4,
+		name : 'Lonnie',
+		username: 'Lonnie',
+		email: 'Lonnie@hotmail.com'
+	},
+	{
+		id:5,
+		name : 'Laura',
+		username: 'lLaura01',
+		email: 'Laura@hotmail.com'
+	},
+	{
+		id:6,
+		name : 'Elmer',
+		username: 'Elmer',
+		email: 'lElmer@hotmail.com'
+	},
+	{
+		id:7,
+		name : 'Henrietta Cress',
+		username: 'HenriettaCress09',
+		email: 'HenriettaCress@hotmail.com'
+	},
+	{
+		id:8,
+		name : 'dave',
+		username: 'dave01',
+		email: 'dave@hotmail.com'
+	},
+	{
+		id:9,
+		name : 'silviya',
+		username: 'lsilviya01',
+		email: 'silviyary@hotmail.com'
+	},
+]
